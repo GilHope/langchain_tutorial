dfa63a9d07dcddd7a95b872d9c63d82f6b1cbd9a
Gil <ghopecs@gmail.com>
2024-01-09 11:05:28 -0500

    add func

--- openai_langchain.py
+++ openai_langchain.py
@@ -4,3 +4,13 @@ from dotenv import load_dotenv
 load_dotenv()
 
 openai.api_key = os.environ.get('OPENAI_API_KEY')
+
+
+def get_completion(prompt, model=llm_model):
+    messages = [{"role": "user", "content": prompt}]
+    response = openai.ChatCompletion.create(
+        model=model,
+        messages=messages,
+        temperature=0, 
+    )
+    return response.choices[0].message["content"]
